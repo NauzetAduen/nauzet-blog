ec5d7dc4163aea379d3675dc0e793d93b0ab68ac
Nauzet Hernández <nauzet.aduen@gmail.com>
2023-04-08 02:35:56 +0100

    initial clean-up

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <p>hola</p>
+
+        {children}
+        <p>adios</p>
+
+      </body>
     </html>
   )
 }
