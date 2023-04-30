9b15b6680b67e8033955757016e2a8d4d60b2cc8
Nauzet Hernández <nauzet.aduen@gmail.com>
2023-04-30 09:56:37 +0100

    Improve home design, Change TextBlock usecase

--- app/home/textBlock.tsx
+++ app/home/textBlock.tsx
@@ -1,17 +1,18 @@
 interface TextBlockParams {
-  title: string;
-  content: string;
+  text: string;
+  highlightedText: string;
 }
 
 export default function TextBlock(props: TextBlockParams) {
-  const { title, content } = props;
-  const lines = content.split("\\n");
+  const { text, highlightedText } = props;
+  const parts = text.split(highlightedText);
   return (
-    <div>
-      <p>{title}</p>
-      {lines.map((line, index) => (
-        <p key={index}>{line}</p>
-      ))}
-    </div>
+    <span className="my-8 border-solid border-2 border-indigo-600 ">
+      {parts[0]}
+      <span className="font-bold bg-gradient-to-tl from-cyan-500 to-red-800 bg-invisible0 bg-no-repeat bg-left-bottom hover:bg-visible3 ease duration-300">
+        {highlightedText}
+      </span>
+      {parts[1]}
+    </span>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,31 @@ import TextBlock from "./home/textBlock";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Nauzet Hernández</h1>
-      <h2>Software Engineer</h2>
-      <p>I develop things and complain at my managers</p>
-      <TextBlock
-        title="What do I do?"
-        content="I’m a Software engineer with experience in the front-end side of things.\nAs a Flutter enthusiast, I love building mobile apps.\nI challenge myself everyday to help the teams I work with, trying to create a healthy and productive enviroment."
-      />
-      <TextBlock
-        title="About me"
-        content="My name is Nauzet, often known as Nau by the masses. I live in Las Palmas de Gran Canaria, Spain.\nI’m a Software engineer by the University of Las Palmas de Gran Canaria, with an intesification in software engineering.\nRecently, I’ve started learning to play the bass, with a special interest in funk bass music.
-I go to the gym a couple of times a week, so.., a work in progress."
-      />
+    <div className="">
+      <h1 className="font-bold text-5xl pt-8">Nauzet Hernández</h1>
+      <h2 className="italic text-lg text-gray-500">
+        Software engineer and Flutter enthusiast
+      </h2>
+      <p className="text-2xl py-8">I tell jokes and code for a living</p>
+      <div className="grid grid-cols-2 gap-4">
+        <TextBlock
+          text="I'm Nauzet, a software engineer and a Flutter enthusiast who loves the front side of things"
+          highlightedText="Nauzet"
+        />
+        <TextBlock
+          text="Aspiring to build a healty and productive enviroment with my teams. Persons over company, you say?"
+          highlightedText="healty and productive"
+        />
+        <TextBlock
+          text="I live in Las Palmas, The Canary Islands, Spain; advocating for a remote work life"
+          highlightedText="Las Palmas"
+        />
+        <TextBlock
+          text="My current interests are the bass, playing chess and working out and as everything in life, it's a work in progress"
+          highlightedText="interests"
+        />
+      </div>
+
       <IconBlock iconPath="" content="" />
       <IconBlock iconPath="" content="" />
       <IconBlock iconPath="" content="" />
