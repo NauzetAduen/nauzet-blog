c39555bebf507bbbc862ae12e30a0e6adaad8da3
Nauzet Hernández <nauzet.aduen@gmail.com>
2023-05-25 00:48:39 +0100

    Add custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="space-x-8 text-center text-base sm:p-40 sm:text-2xl">
+      <span>404</span>
+      <span>|</span>
+      <span>Resource not found</span>
+    </div>
+  );
+}
